3d9b0ca794a76aed1388a9a02808380e6d9811ba
lorenzorb <lorenzo.buglione@anchieta.br>
2022-05-20 17:19:16 -0300

    AppComplete :zap:

--- src/screens/Repository/index.tsx
+++ src/screens/Repository/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRoute } from '@react-navigation/core';
+import { useRoute } from '@react-navigation/native';
 import { Linking } from 'react-native';
 import { useRepositories } from '../../hooks/useRepositories';
 
@@ -38,23 +38,28 @@ export function Repository() {
 
   function handleIssueNavigation(issueUrl: string) {
     // TODO - use Linking to open issueUrl in a browser
+    Linking.openURL(issueUrl);
   }
+
+  // console.log(repository)
 
   return (
     <Background>
       <Container>
         <RepoInfo>
-          {/* <OwnerAvatar source={{ uri:  }} /> */}
+          <OwnerAvatar source={{ uri:  repository.owner.avatar_url }} />
 
           <TextGroup>
             <TitleAnimation>
               {
                 // TODO - full name of the repository
+                repository.full_name
               }
             </TitleAnimation>
 
             <Description numberOfLines={2}>{
               //TODO - repository description
+              repository.description
             }</Description>
           </TextGroup>
         </RepoInfo>
@@ -63,6 +68,7 @@ export function Repository() {
           <Stars>
             <StarsCounter>{
               // TODO - repository stargazers count
+              repository.stargazers_count
             }</StarsCounter>
             <StarsText>Stars</StarsText>
           </Stars>
@@ -70,6 +76,7 @@ export function Repository() {
           <Forks>
             <ForksCounter>{
               // TODO - repository forks count
+              repository.forks_count
             }</ForksCounter>
             <ForksText>Forks</ForksText>
           </Forks>
@@ -77,6 +84,7 @@ export function Repository() {
           <OpenIssues>
             <OpenIssuesCounter>{
               // TODO - repository issues count
+              repository.open_issues_count
             }</OpenIssuesCounter>
             <OpenIssuesText>Issues{'\n'}Abertas</OpenIssuesText>
           </OpenIssues>
@@ -94,6 +102,7 @@ export function Repository() {
                 subTitle: issue.user.login,
               }}
             // TODO - onPress prop calling 
+            onPress={() => handleIssueNavigation(issue.html_url)}
             />
           )}
         />
